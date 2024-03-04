7ae7494ccbe06d542c84717c292ee608d6592c6d
Malik <elmelz6472@gmail.com>
2024-03-04 00:26:56 -0500

    continued

--- crawlers/indeed_crawler.py
+++ crawlers/indeed_crawler.py
@@ -1,11 +1,19 @@
 import os
 import yaml
 import time
+import threading
 from playwright.sync_api import sync_playwright
-from controller.scraper_controller import ScraperController  # Adjust the import path
+
+#I DONT KNOW WHY IT WOKRS BUT IT WOKRS
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from controllers.controller_sraper import ScraperController
+
+DISMISS_BUTTON = "#mosaic-desktopserpjapopup > div.css-g6agtu.eu4oa1w0 > button"
 
 class IndeedScraper:
-    def __init__(self, config_path):
+    def __init__(self, config_path, depth=5):
         with open(config_path, 'r') as file:
             config = yaml.safe_load(file)
         self.url = config['INDEED_URL']
@@ -14,6 +22,7 @@ class IndeedScraper:
         self.input_fields = config.get('input_fields', {})
         self.clickable_fields = config.get('button_fields', {})
         self.controller = None
+        self.depth = depth
 
     def run(self):
         with sync_playwright() as p:
@@ -21,34 +30,48 @@ class IndeedScraper:
 
             time.sleep(1)
 
-            self.scroll_to_bottom_smooth(page)
 
-            time.sleep(1)
+            for page_idx in range(1, self.depth):
+                self.scroll_and_check_element(page)
 
-            self.store_page_as_html(page, "page.html")
+                time.sleep(1)
+                self.store_page_as_html(page, f"page{page_idx}.html")
+                self.click_next_button()
 
             browser.close()
 
-    def setup_crawler(self, playwright):
-        # Launch the browser
-        browser = playwright.chromium.launch(headless=self.isHeadless)
-        # Create a new page
-        page = browser.new_page()
-        # Set default navigation timeout
-        page.set_default_navigation_timeout(0)
-        # Set extra HTTP headers
-        page.set_extra_http_headers({"User-Agent": self.user_agent})
-        # Navigate to the URL
-        page.goto(self.url)
 
+
+    def scroll_and_check_element(self, page):
+        start_time = time.time()
+        timeout = 5
+        while not self.is_element_visible(page, DISMISS_BUTTON):
+            if time.time() - start_time > timeout:
+                print("Element did not appear within the timeout period.")
+                break
+            time.sleep(0.5)
+
+        self.scroll_to_element(page, 'pagination-page-next')
+
+    def is_element_visible(self, page, selector):
+        return page.is_visible(selector)
+
+    def handle_popup(self, selector):
+        self.controller.click(selector)
+
+
+    def setup_crawler(self, playwright):
+        browser = playwright.chromium.launch(headless=self.isHeadless)
+        page = browser.new_page()
+        page.set_default_timeout(60000)
+        page.set_extra_http_headers({"User-Agent": self.user_agent})
+        page.goto(self.url)
 
         self.setup_scraper_controller(page)
 
         time.sleep(1)
 
-
         self.enter_data()
-
 
         self.click_submit_button()
 
@@ -72,7 +95,6 @@ class IndeedScraper:
             print(f"Selector or data not found for {field_type} field in config.yaml")
 
     def click_submit_button(self):
-        # Click on the submit button
         selector = self.clickable_fields.get('submit_button', {}).get('selector', '')
         if selector:
             print(f"Clicking on submit button with selector '{selector}'")
@@ -80,15 +102,48 @@ class IndeedScraper:
         else:
             print("Selector not found for submit button in config.yaml")
 
+    def click_next_button(self):
+        selector = self.clickable_fields.get('next_button', {}).get('selector', '')
+        if selector:
+            print(f"Clicking on next button with selector '{selector}'")
+            self.controller.click(selector, custom_query="data-testid='myButton'")
+        else:
+            print("Selector not found for next button in config.yaml")
+
+
     def scroll_to_bottom_smooth(self, page):
         page.evaluate('(async () => { await new Promise(resolve => { let totalHeight = 0; let distance = 100; let timer = setInterval(() => { let scrollHeight = document.body.scrollHeight; window.scrollBy(0, distance); totalHeight += distance; if(totalHeight >= scrollHeight){ clearInterval(timer); resolve(); } }, 50); }); })()')
 
+    def scroll_to_element(self, page, test_id):
+            script = f'''
+                (async () => {{
+                    const targetElement = document.querySelector('[data-testid="{test_id}"]');
+                    if (!targetElement) {{
+                        console.error('Target element not found with data-testid:', '{test_id}');
+                        return;
+                    }}
+
+                    await new Promise(resolve => {{
+                        let distance = 100;
+                        let timer = setInterval(() => {{
+                            let rect = targetElement.getBoundingClientRect();
+                            if (rect.top >= 0 && rect.bottom <= window.innerHeight) {{
+                                clearInterval(timer);
+                                resolve();
+                            }} else {{
+                                window.scrollBy(0, distance);
+                            }}
+                        }}, 50);
+                    }});
+                }})();
+            '''
+            page.evaluate(script)
+
+
+
     def store_page_as_html(self, page, file_name):
-        # Get the current HTML content of the page
         html_content = page.content()
-        # Define the path to store the HTML file
         file_path = os.path.join("html", file_name)
-        # Write the HTML content to the file
         with open(file_path, "w", encoding="utf-8") as file:
             file.write(html_content)
         print(f"Page HTML stored as {file_path}")
--- parsers/indeed_parser.py
+++ parsers/indeed_parser.py
@@ -1,65 +1,46 @@
 from bs4 import BeautifulSoup
-import pprint
+import json
+
+class HTMLParser:
+    """Return a list of companies name based on a given html"""
+    def __init__(self, html_file):
+        self.html_file = html_file
+        self.company_info_list = None
+
+    def extract_company_info(self):
+        with open(self.html_file, 'r', encoding='utf-8') as file:
+            soup = BeautifulSoup(file, 'html.parser')
+            company_info_list = []
+
+            job_items = soup.find_all('div', {'class': 'slider_item'})
+            for job_item in job_items:
+                company_name = job_item.find('span', {'data-testid': 'company-name'}).get_text(strip=True)
+                location = job_item.find('div', {'data-testid': 'text-location'}).get_text(strip=True)
+
+                company_info_list.append({"name": company_name, "location": location})
+
+            return company_info_list
+
+    def pretty_print_company_info(self):
+        if self.company_info_list is None:
+            self.company_info_list = self.extract_company_info()
+
+        print("Company Info:")
+        for company_info in self.company_info_list:
+            print(f"Name: {company_info['name']}, Location: {company_info['location']}")
+
+    def export_to_json(self, output_file):
+        if self.company_info_list is None:
+            self.company_info_list = self.extract_company_info()
+
+        with open(output_file, 'w', encoding='utf-8') as json_file:
+            json.dump(self.company_info_list, json_file, indent=4, ensure_ascii=False)
+
+    def run(self):
+        self.pretty_print_company_info()
 
 
-def print_tables_without_role_presentation(file_path, ul_selector):
-    with open(file_path, 'r', encoding='utf-8') as file:
-        soup = BeautifulSoup(file, 'html.parser')
-        ul = soup.select_one(ul_selector)
-        if ul:
-            table_tags = ul.find_all('table', recursive=True)  # Search only immediate children
-            if table_tags:
-                for table in table_tags:
-                    # if not table.has_attr('role'):
-                        print(table.prettify())
-            else:
-                print(f"No <table> elements found that are children of '{ul_selector}'")
-        else:
-            print(f"No <ul> element found with selector '{ul_selector}'")
-
-
-def parse_li_elements(file_path):
-    with open(file_path, 'r', encoding='utf-8') as file:
-        soup = BeautifulSoup(file, 'html.parser')
-        li_elements = soup.find_all('li')
-        for i, li in enumerate(li_elements):
-            # Get the text content of the li element
-            text = li.get_text(strip=True)
-            # Get the href attribute value if it exists
-            href = li.find('a')['href'] if li.find('a') else None
-            # Print the text content and href attribute value
-            print(f"Text: {text}, Href: {href}")
-            # Print a separator line between entries except for the last one
-            if i < len(li_elements) - 1:
-                print("-" * 40)
-
-
-def parse_html_file(file_path):
-    # Read HTML content from file
-    with open(file_path, "r", encoding="utf-8") as file:
-        html_content = file.read()
-
-    soup = BeautifulSoup(html_content, "html.parser")
-
-    # Find all <li> elements
-    list_items = soup.find_all("li", class_="css-5lfssm eu4oa1w0")
-
-    for item in list_items:
-        h2_tags = item.find_all("h2", class_="jobTitle css-14z7akl eu4oa1w0")
-        for h2_tag in h2_tags:
-            job_title = h2_tag.text.strip()
-            job_href = h2_tag.a['href'] if h2_tag.a else "N/A"
-            print("Job Title:", job_title)
-            print("Job Href:", job_href)
-            print()
-
-
-# Example usage:
-file_path = 'html/test.html'
-ul_selector = '#mosaic-provider-jobcards > ul'
-
-jobs = parse_html_file(file_path)
-
-# Pretty print the jobs
-# pp = pprint.PrettyPrinter(indent=4)
-# pp.pprint(jobs)
+html_file = 'html/page.html'
+parser = HTMLParser(html_file)
+parser.run()
+parser.export_to_json('results/companies.json')
